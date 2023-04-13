d79aa4e1b92a7165044bc6fe0cf0ced2756098ba
hyojine <touroz@naver.com>
2023-04-13 12:50:40 +0900

    feat: 회원가입 테스트코드 완성

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SignUpTest(APITestCase):
+    def test_signup(self):
+        url=reverse('signup_view')
+        user_data={
+            'username':'test',
+            'email':'test@test.com',
+            'password':'1234',
+            'password_check':'1234'
+        }
+        response=self.client.post(url,user_data)
+        print(response.data)
+        self.assertEqual(response.data['message'],"회원가입이 완료되었습니다!")
