e4863be5949179cba930929738a2777dfad4eec2
hyojine <touroz@naver.com>
2023-04-13 23:58:54 +0900

    feat:로그인 기능 테스트 코드 작성

--- user/tests.py
+++ user/tests.py
@@ -1,8 +1,9 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from .models import Users
 
 
-class SignUpTest(APITestCase):
+class UserSignupTest(APITestCase):
     def test_signup(self):
         url=reverse('signup_view')
         user_data={
@@ -11,6 +12,20 @@ class SignUpTest(APITestCase):
             'password':'1234',
             'password_check':'1234'
         }
-        response=self.client.post(url,user_data)
-        print(response.data)
-        self.assertEqual(response.data['message'],"회원가입이 완료되었습니다!")
+        response=self.client.post(url,user_data) 
+        self.assertEqual(response.status_code,201)
+    
+class UserLoginTest(APITestCase):    
+    def setUp(self):
+        self.user=Users.objects.create_user(email='test@test.com',username='username',password='1234') #변수 없어도 됨
+        self.data = {   
+                        'username':'test',
+                        'email':'test@test.com',
+                        'password':'1234',
+                        'password_check':'1234'
+                    }
+
+    def test_login(self):
+        url=reverse('login_view')
+        response=self.client.post(url,self.data) 
+        self.assertEqual(response.status_code,200)
